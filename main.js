9e0e20a495cc7839eea198449579cf65bf35118a
Marco Pretelin <marcoapretelin.igf@gmail.com>
2023-06-02 14:56:37 -0600

    Changes in css style and html dcoument.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const d = document;
 
 
 
-function hamburgerMenu (panelBtn, panel, panelClose, section){
+function hamburgerMenu (panelBtn, panel, panelClose, section, checkBtn, checkPanel){
 
     d.addEventListener('click', (e) =>{
 
@@ -10,6 +10,12 @@ function hamburgerMenu (panelBtn, panel, panelClose, section){
 
             d.querySelector(panel).classList.toggle('is-active'); 
             d.querySelector(panelBtn).classList.toggle('is-active');
+
+            d.querySelector(checkPanel).classList.remove('is-active'); 
+            d.querySelector(checkBtn).classList.remove('is-active');
+
+           
+            
         }
 
         if(e.target.matches(panelClose) || e.target.matches(`${panelClose} *`)) {
@@ -28,6 +34,28 @@ function hamburgerMenu (panelBtn, panel, panelClose, section){
     });
 }
 
-hamburgerMenu('.open-menu', '.panel', '.close', '.section');
+
+function Checkcart(checkBtn, checkPanel, section){
+
+    d.addEventListener('click', (e) =>{
+
+        if(e.target.matches(checkBtn) || e.target.matches(`${checkBtn} *`)) {
+
+            d.querySelector(checkPanel).classList.toggle('is-active'); 
+            d.querySelector(checkBtn).classList.toggle('is-active');
+        }
+
+
+
+        if(e.target.matches(section) || e.target.matches(`${section} *`)) {
+
+            d.querySelector(checkPanel).classList.remove('is-active'); 
+            d.querySelector(checkBtn).classList.remove('is-active');
+        }
+    })
+}
+
+hamburgerMenu('.open-menu', '.panel', '.close', '.section', '.open-cart', '.checkout-panel');
+Checkcart('.open-cart', '.checkout-panel', '.section');
 
 
