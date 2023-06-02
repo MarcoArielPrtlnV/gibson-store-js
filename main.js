0725d0cafad96a368f6080113a3def686ce25b13
Marco Pretelin <marcoapretelin.igf@gmail.com>
2023-06-01 22:51:20 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const d = document;
+
+
+
+function hamburgerMenu (panelBtn, panel, panelClose, section){
+
+    d.addEventListener('click', (e) =>{
+
+        if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+
+            d.querySelector(panel).classList.toggle('is-active'); 
+            d.querySelector(panelBtn).classList.toggle('is-active');
+        }
+
+        if(e.target.matches(panelClose) || e.target.matches(`${panelClose} *`)) {
+
+            d.querySelector(panel).classList.remove('is-active'); 
+            d.querySelector(panelBtn).classList.remove('is-active');
+        }
+
+        if(e.target.matches(section) || e.target.matches(`${section} *`)) {
+
+            d.querySelector(panel).classList.remove('is-active'); 
+            d.querySelector(panelBtn).classList.remove('is-active');
+        }
+
+        
+    });
+}
+
+hamburgerMenu('.open-menu', '.panel', '.close', '.section');
+
+
